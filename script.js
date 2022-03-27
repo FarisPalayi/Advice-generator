147c57097af6a4f6ac84a7ad8f9057587179923f
FarisPalayi <farispalayi@gmail.com>
2022-03-27 23:38:06 +0530

    chore: disable chache to show random advices everytime

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const adviceIdElm = document.querySelector("[data-advice-id]");
 const adviceBtnElm = document.querySelector("[data-advice-generate-btn]");
 
 async function showRandomAdvice() {
-  await fetch("https://api.adviceslip.com/advice")
+  await fetch("https://api.adviceslip.com/advice", { cache: "no-cache" })
     .then((res) => res.json())
     .then((data) => {
       adviceElm.innerText = data.slip.advice;
       adviceIdElm.innerText = `Advice #${data.slip.id}`;
-    })
+    });
 }
 
 adviceBtnElm.addEventListener("click", showRandomAdvice);
