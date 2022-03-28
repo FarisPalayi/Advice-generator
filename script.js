2b815523e90ceb3bc33794b52a700dab25b4c601
FarisPalayi <farispalayi@gmail.com>
2022-03-28 16:44:45 +0530

    chore: first loading concept (dummy)

--- script.js
+++ script.js
@@ -2,15 +2,31 @@ const adviceElm = document.querySelector("[data-advice]");
 const adviceIdElm = document.querySelector("[data-advice-id]");
 const adviceBtnElm = document.querySelector("[data-advice-generate-btn]");
 
+let loading = false;
+
 async function showRandomAdvice() {
+  if (loading) return;
+
+  runLoader();
+  
   await fetch("https://api.adviceslip.com/advice", { cache: "no-cache" })
     .then((res) => res.json())
     .then((data) => {
       adviceElm.innerText = data.slip.advice;
       adviceIdElm.innerText = `Advice #${data.slip.id}`;
+      loading = false;
     });
 }
 
 adviceBtnElm.addEventListener("click", showRandomAdvice);
 
 showRandomAdvice();
+
+
+function runLoader() {
+  loading = true;
+  adviceElm.innerText = "Loading...";
+  adviceIdElm.innerText = "";
+}
+
+runLoader();
