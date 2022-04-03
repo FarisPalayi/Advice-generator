fb3a1b31d0dd8670d46ee47ece9499eea0a7b248
FarisPalayi <farispalayi@gmail.com>
2022-04-04 00:36:46 +0530

    Revert "chore: timeout request after 8 seconds"
    This reverts commit 3ac04b4b805bc80583ce285782fc40ca0397a9a3.

--- script.js
+++ script.js
@@ -13,46 +13,32 @@ const errorRetryBtnElm = qs("[data-error-retry-btn]");
 
 adviceBtnElm.disabled = false;
 
-async function fetchWithTimeout(
-  url,
-  options = { timeout: 8000, cache: "no-cache" }
-) {
-  const { timeout } = options;
-
-  const controller = new AbortController();
-  const timeoutId = setTimeout(() => controller.abort(), timeout);
-  const response = await fetch(url, { ...options, signal: controller.signal });
-
-  clearTimeout(timeoutId);
-  return response;
-}
-
 const getRandomAdvice = async () => {
-  const res = await fetchWithTimeout("https://api.adviceslip.com/advice");
+  const res = await fetch("https://api.adviceslip.com/advice", { cache: "no-cache" });
   const randomAdvice = await res.json();
   return randomAdvice;
-};
+}
 
 const showLoader = () => {
   loaderElm.style.display = "block";
   loaderMsgElm.innerText = "Loading advice..."; // for `aria-live` to work
   adviceElm.classList.add("sr-only"); // not using `display: none`, cuz,
   adviceIdElm.classList.add("sr-only"); // it will mess up the screen reader's ability to read the text
-};
+}
 
 const hideLoader = () => {
   loaderElm.style.display = "none";
   loaderMsgElm.innerText = "";
   adviceElm.classList.remove("sr-only");
   adviceIdElm.classList.remove("sr-only");
-};
+}
 
 const hideSnackbar = () => {
   errorSnackbarElm.style.transition = "opacity 0s";
   errorSnackbarElm.style.opacity = "0";
   errorRetryBtnElm.style.display = "none";
   errorSnackbarMsgElm.innerText = "";
-};
+}
 
 const showSnackbar = (errMsg = "An error occurred. Please try again.") => {
   errorSnackbarElm.style.transition = "opacity 0.2s";
@@ -60,15 +46,13 @@ const showSnackbar = (errMsg = "An error occurred. Please try again.") => {
   errorSnackbarMsgElm.innerText = errMsg;
   errorRetryBtnElm.style.display = "inline-block";
   errorRetryBtnElm.focus();
-};
+}
 
-const showRandomAdvice = (initialCb, successCb, errorCb) => {
-  // cb = callback
+const showRandomAdvice = (initialCb, successCb, errorCb) => { // cb = callback
   initialCb();
 
   getRandomAdvice()
-    .then((data) => {
-      // data validation is omitted for now
+    .then((data) => { // data validation is omitted for now
       adviceElm.innerText = `“${data.slip.advice}”`;
       adviceIdElm.innerText = `Advice #${data.slip.id}`;
       successCb();
@@ -77,7 +61,7 @@ const showRandomAdvice = (initialCb, successCb, errorCb) => {
       console.error(err);
       errorCb();
     });
-};
+}
 
 const showRandomAdviceWrapper = () =>
   showRandomAdvice(
